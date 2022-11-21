c8daf2f39e5bed817604a1687c3957efc88ab256
denzelcrocker <zelentsov36@gmail.com>
2022-11-21 14:09:48 +0300

    Added layouts

---
+++ app/src/main/java/com/example/apihumans/Add_timetable.java
@@ -0,0 +1,14 @@
+package com.example.apihumans;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_timetable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_timetable);
+    }
+}
